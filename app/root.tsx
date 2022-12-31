24ff4d50a6575147c9986b5ff8dfbe70c508d08f
Todd Goates <tgoates@q90.com>
2022-12-31 16:32:37 -0700

    Add 404 page.

--- app/root.tsx
+++ app/root.tsx
@@ -1,11 +1,13 @@
 import type { MetaFunction } from "@remix-run/node";
 import {
   Links,
+  Link,
   LiveReload,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
+  useCatch,
 } from "@remix-run/react";
 import Navbar from "./components/Navbar";
 import styles from "./styles/app.css";
@@ -20,22 +22,77 @@ export function links() {
   return [{ rel: "stylesheet", href: styles }];
 }
 
-export default function App() {
+function Document({
+  children,
+  title = "Todd Goates | Utah-Based, Full-Stack Developer",
+}: {
+  children: React.ReactNode;
+  title?: string;
+}) {
   return (
     <html lang="en" className="h-full bg-gray-100">
       <head>
-        <Meta />
+        <meta charSet="utf-8" />
+        <title>{title}</title>
         <Links />
       </head>
-      <body className="h-full">
+      <body>
         <Navbar />
-        <main>
-          <Outlet />
-        </main>
+        <main>{children}</main>
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
       </body>
     </html>
+  );
+}
+
+export function CatchBoundary() {
+  const caught = useCatch();
+
+  if (caught.status === 404) {
+    return (
+      <Document title={`${caught.status} ${caught.statusText}`}>
+        <div className="p-5 flex flex-col gap-8">
+          <h1 className="text-4xl font-bold">Oh No!</h1>
+          <p className="text-lg">
+            It looks like you tried to go to a page that doesn't exist!
+          </p>
+          <Link
+            to="/"
+            className="text-lg text-purple-500 hover:text-purple-700"
+          >
+            Go Home
+          </Link>
+          <img
+            src="images/computer-cat.jpg"
+            alt="A gray and white ragdoll cat sitting on a chair in front of a laptop"
+            className="w-full"
+          />
+        </div>
+      </Document>
+    );
+  }
+
+  return (
+    <Document title={`${caught.status} ${caught.statusText}`}>
+      <div className="p-5 flex flex-col gap-8">
+        <h1 className="text-4xl font-bold">Oh No!</h1>
+        <p className="text-lg">
+          Something went wrong with this site. Don't worry, we're on it!
+        </p>
+        <Link to="/" className="text-lg text-purple-500 hover:text-purple-700">
+          Go Home
+        </Link>
+      </div>
+    </Document>
+  );
+}
+
+export default function App() {
+  return (
+    <Document>
+      <Outlet />
+    </Document>
   );
 }
