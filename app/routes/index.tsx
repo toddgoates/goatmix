b812358238ecb3e1de0888f8f4dcb1682c38be8b
Todd Goates <tgoates@q90.com>
2023-01-04 13:40:17 -0700

    Add Jumbotron component to keep the code on the home page a bit cleaner.

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,4 +1,5 @@
 import Typist from "react-typist-component";
+import Jumbotron from "~/components/Jumbotron";
 import ButtonLink from "~/components/ButtonLink";
 
 export default function Index() {
@@ -19,40 +20,32 @@ export default function Index() {
   ];
 
   return (
-    <section
-      className="flex h-full p-5"
-      style={{
-        background: "f3f4f6",
-        backgroundImage: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='36' height='72' viewBox='0 0 36 72'%3E%3Cg fill-rule='evenodd'%3E%3Cg fill='%23c6bed3' fill-opacity='0.25'%3E%3Cpath d='M2 6h12L8 18 2 6zm18 36h12l-6 12-6-12z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E")`,
-      }}
-    >
-      <div className="flex flex-col gap-12 m-auto text-center">
-        <h2 className="text-6xl">Hey, I'm Todd!</h2>
-        <h3 className="flex items-center justify-center gap-2 text-3xl">
-          <span>I'm a</span>
-          <span className="p-1 font-bold text-white bg-purple-500">
-            <Typist
-              typingDelay={175}
-              finishDelay={500}
-              cursor={<span className="h-1 ml-1 bg-white">|</span>}
-            >
-              {words.map((word, index) => (
-                <div key={word}>
-                  <span>{word}</span>
-                  <Typist.Delay ms={500} />
-                  {index + 1 === words.length ? (
-                    <span></span>
-                  ) : (
-                    <Typist.Backspace count={word.length} />
-                  )}
-                </div>
-              ))}
-            </Typist>
-          </span>
-        </h3>
-        <p className="text-lg">I love to build cool stuff with code</p>
-        <ButtonLink to="/about" text="Learn More" />
-      </div>
-    </section>
+    <Jumbotron>
+      <h2 className="text-6xl">Hey, I'm Todd!</h2>
+      <h3 className="flex items-center justify-center gap-2 text-3xl">
+        <span>I'm a</span>
+        <span className="p-1 font-bold text-white bg-purple-500">
+          <Typist
+            typingDelay={175}
+            finishDelay={500}
+            cursor={<span className="h-1 ml-1 bg-white">|</span>}
+          >
+            {words.map((word, index) => (
+              <div key={word}>
+                <span>{word}</span>
+                <Typist.Delay ms={500} />
+                {index + 1 === words.length ? (
+                  <span></span>
+                ) : (
+                  <Typist.Backspace count={word.length} />
+                )}
+              </div>
+            ))}
+          </Typist>
+        </span>
+      </h3>
+      <p className="text-lg">I love to build cool stuff with code</p>
+      <ButtonLink to="/about" text="Learn More" />
+    </Jumbotron>
   );
 }
