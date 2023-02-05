8d22e886f8c1b4a6d4336ca9c163619bf49b5984
Todd Goates <toddgoates@gmail.com>
2023-02-04 18:59:29 -0700

    Update ProjectCard images to have links

--- app/components/ProjectCard.tsx
+++ app/components/ProjectCard.tsx
@@ -14,13 +14,15 @@ export default function ProjectCard({
   return (
     <div key={project.name}>
       <div className="flex flex-col gap-8 md:flex-row">
-        <img
-          src={`images/${project.image}`}
-          alt={`Screenshot of ${project.name}`}
-          className={`w-full h-full md:w-1/2 lg:w-1/3 xl:w-1/4 ${
-            reverse ? "md:order-last" : "md:order-first"
-          }`}
-        />
+        <a href={project.link} target="_blank" rel="noreferrer">
+          <img
+            src={`images/${project.image}`}
+            alt={`Screenshot of ${project.name}`}
+            className={`w-full h-full md:w-1/2 lg:w-1/3 xl:w-1/4 ${
+              reverse ? "md:order-last" : "md:order-first"
+            }`}
+          />
+        </a>
         <div className="grow">
           <div className="flex flex-col gap-8">
             <h3 className="text-xl font-semibold">{project.name}</h3>
