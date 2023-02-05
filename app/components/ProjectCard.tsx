e69340ce016c5e1dd2d38167320ce1719827844a
Todd Goates <toddgoates@gmail.com>
2023-02-04 19:00:57 -0700

    Fix CSS bug

--- app/components/ProjectCard.tsx
+++ app/components/ProjectCard.tsx
@@ -14,15 +14,13 @@ export default function ProjectCard({
   return (
     <div key={project.name}>
       <div className="flex flex-col gap-8 md:flex-row">
-        <a href={project.link} target="_blank" rel="noreferrer">
-          <img
-            src={`images/${project.image}`}
-            alt={`Screenshot of ${project.name}`}
-            className={`w-full h-full md:w-1/2 lg:w-1/3 xl:w-1/4 ${
-              reverse ? "md:order-last" : "md:order-first"
-            }`}
-          />
-        </a>
+        <img
+          src={`images/${project.image}`}
+          alt={`Screenshot of ${project.name}`}
+          className={`w-full h-full md:w-1/2 lg:w-1/3 xl:w-1/4 ${
+            reverse ? "md:order-last" : "md:order-first"
+          }`}
+        />
         <div className="grow">
           <div className="flex flex-col gap-8">
             <h3 className="text-xl font-semibold">{project.name}</h3>
